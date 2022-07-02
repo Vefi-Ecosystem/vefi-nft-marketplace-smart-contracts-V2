55e2382ecd7c5a1ccc6f927667215d1f2b4f3d9d
Kingsley Victor <javaprodigy56@gmail.com>
2022-07-02 22:48:45 +0100

    update collection contract

--- contracts/Collection.sol
+++ contracts/Collection.sol
@@ -2,26 +2,34 @@ pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/access/AccessControl.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
 import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
 import '@openzeppelin/contracts/utils/math/SafeMath.sol';
 
-contract Collection is ERC721URIStorage, ERC721Enumerable, Ownable, ReentrancyGuard, AccessControl {
+contract Collection is ERC721URIStorage, ERC721Enumerable, ERC721Royalty, Ownable, ReentrancyGuard, AccessControl {
   using Counters for Counters.Counter;
   using SafeMath for uint256;
 
   Counters.Counter private tokenIds;
 
   uint256 public maxSupply;
-  uint256 public currentSupply;
   uint256 public mintStartTime;
 
   bytes32 public minterRole = keccak256(abi.encode('MINTER_ROLE'));
+  bytes32 public royaltySetterRole = keccak256(abi.encode('ROYALTY_SETTER_ROLE'));
+
+  uint96 public royaltyNumerator = 2000;
 
   modifier ownerOrMinter() {
     require(owner() == _msgSender() || hasRole(minterRole, _msgSender()), 'must_be_owner_or_minter');
+    _;
+  }
+
+  modifier onlyRoyaltySetter() {
+    require(hasRole(royaltySetterRole, _msgSender()), 'only_royalty_setter');
     _;
   }
 
@@ -35,25 +43,24 @@ contract Collection is ERC721URIStorage, ERC721Enumerable, Ownable, ReentrancyGu
     maxSupply = maxSupply_;
     mintStartTime = mintStartTime_;
     _grantRole(minterRole, _msgSender());
+    _grantRole(royaltySetterRole, _msgSender());
     _transferOwnership(owner_);
+    _grantRole(royaltySetterRole, owner_);
   }
 
   function mint(address _to, string memory _tokenURI) external nonReentrant ownerOrMinter returns (uint256 tokenId) {
-    require(currentSupply <= maxSupply, 'cannot_exceed_maximum_number_of_items_in_collection');
+    require(totalSupply() <= maxSupply, 'cannot_exceed_maximum_number_of_items_in_collection');
     require(block.timestamp >= mintStartTime, 'not_open_for_minting');
     tokenIds.increment();
     tokenId = tokenIds.current();
     _safeMint(_to, tokenId);
     _setTokenURI(tokenId, _tokenURI);
-
-    currentSupply = currentSupply.add(1);
+    _setTokenRoyalty(tokenId, _to, royaltyNumerator);
   }
 
   function burn(uint256 _tokenId) external nonReentrant onlyOwner {
     require(_exists(_tokenId), 'token_must_exist');
     _burn(_tokenId);
-
-    currentSupply = currentSupply.sub(1);
   }
 
   function addMinter(address _minter) external onlyOwner {
@@ -70,13 +77,13 @@ contract Collection is ERC721URIStorage, ERC721Enumerable, Ownable, ReentrancyGu
     public
     view
     virtual
-    override(AccessControl, ERC721, ERC721Enumerable)
+    override(AccessControl, ERC721, ERC721Enumerable, ERC721Royalty)
     returns (bool)
   {
     return interfaceId == type(IAccessControl).interfaceId || super.supportsInterface(interfaceId);
   }
 
-  function _burn(uint256 tokenId) internal virtual override(ERC721, ERC721URIStorage) {
+  function _burn(uint256 tokenId) internal virtual override(ERC721, ERC721URIStorage, ERC721Royalty) {
     return super._burn(tokenId);
   }
 
@@ -90,5 +97,17 @@ contract Collection is ERC721URIStorage, ERC721Enumerable, Ownable, ReentrancyGu
 
   function tokenURI(uint256 tokenId) public view virtual override(ERC721, ERC721URIStorage) returns (string memory) {
     return super.tokenURI(tokenId);
+  }
+
+  function setRoyalty(uint96 royalty) external onlyRoyaltySetter returns (bool) {
+    royaltyNumerator = royalty;
+
+    if (totalSupply() > 0) {
+      for (uint256 i = 0; i < totalSupply(); i++) {
+        (address receiver, ) = royaltyInfo(tokenByIndex(i), 0);
+        _setTokenRoyalty(tokenByIndex(i), receiver, royalty);
+      }
+    }
+    return true;
   }
 }
